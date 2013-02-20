d5ac5cc4dec79845e94ef1ae3e3415f2d13cb492
Adam <adamb3189@gmail.com>
2013-02-21 09:17:23 +1100

    Added additional comments to the URL class
    Added comments in order to clarify what exactly is happening with some of the more complicated aspects. 
    namely the base_convert(rand()) call and the $rules variable.  

--- application/models/Url.php
+++ application/models/Url.php
@@ -1,15 +1,13 @@
 <?php
 /**
- * Created by IntelliJ IDEA.
  * User: AdamBeck
  * Date: 19/02/13
  * Time: 12:00 PM
- * To change this template use File | Settings | File Templates.
  */ 
 class Url extends Eloquent{
 
     public static $rules = array(
-        'url' => 'required|url'
+        'url' => 'required|url' // the url field is requied and must be a valid url. 
     );
 
     public static function validate($input){
@@ -20,10 +18,10 @@ class Url extends Eloquent{
     }
 
     public static function get_unique_short_url(){
-        $shortened = base_convert(rand(10000,99999), 10, 36); //should be base 64
+        $shortened = base_convert(rand(10000,99999), 10, 36); //generates a random number & converts to characters.should be base 64
         if(static::where_shortened($shortened)->first()){
         // if new shortened url matches an existing shortened url
-            return static::get_unique_short_url(); //make a new one
+            return static::get_unique_short_url(); //repeat above steps and make a new url
         }
         return $shortened; //otherwise, return it.
     }
