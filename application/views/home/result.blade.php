118cdea72636c2c14934e9376f5c90adb0a544d3
Adam <adamb3189@gmail.com>
2013-02-21 09:32:54 +1100

    Added comments to explain HTML class

--- application/views/home/result.blade.php
+++ application/views/home/result.blade.php
@@ -1,5 +1,7 @@
 @layout('master')
     @section('container')
         <h1>Here is your short URL</h1>
-        {{ HTML::link($shortened, "url.dev/$shortened") }}
+        {{ HTML::link($shortened, "url.dev/$shortened") }} <!-- use Laravel HTML class to show a link -->
+        <!-- url text will display the value of the shortened URL --> 
+        <!-- Value will be the same. -->
     @endsection
